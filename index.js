44aebb52aacdbf1712effc9e7d9decabd262ba1b
tommelo <neilton@commerce-connector.com>
2020-10-12 21:45:23 +0200

    Added module exporter

---
+++ index.js
@@ -0,0 +1,5 @@
+const { Socket } = require("./lib/Socket"),
+      { SocketError } = require("./lib/SocketError"),
+      { SocketErrorType } = require("./lib/SocketErrorType");
+
+module.exports = { Socket, SocketError, SocketErrorType };
